524ffe29f7eeed8342d3f369ad58e71a8ba6d545
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-09-02 15:18:14 +0300

    PingPongServer class added

---
+++ PingPong/PingPongServer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace PingPong
+{
+    public class PingPongServer
+    {
+        private TcpListener server;
+        private IPAddress ipAddress = Dns.GetHostEntry("localhost").AddressList[0];
+        TcpClient client;
+        public PingPongServer(int port)
+        {
+            server = new TcpListener(ipAddress, port);
+        }
+        public void Run()
+        {
+            while(true)
+            {
+                //---incoming client connected---
+                client = server.AcceptTcpClient();
+
+                //---get the incoming data through a network stream---
+                NetworkStream nwStream = client.GetStream();
+                byte[] buffer = new byte[client.ReceiveBufferSize];
+
+                //---read incoming stream---
+                int bytesRead = nwStream.Read(buffer, 0, client.ReceiveBufferSize);
+
+                //---convert the data received into a string---
+                string dataReceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                if (dataReceived.ToLower() == "exit")
+                {
+                    break;
+                }
+                Console.WriteLine("Received : " + dataReceived);
+
+                //---write back the text to the client---
+                Console.WriteLine("Sending back : " + dataReceived);
+                nwStream.Write(buffer, 0, bytesRead);
+                client.Close();
+            }
+            server.Stop();
+        }
+    }
+}
